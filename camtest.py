849b2fd958dc024f42f5e426ffb28b23b6bf89ec
6Tgraybird <107027564+6Tgraybird@users.noreply.github.com>
2023-07-16 22:57:02 +0530

    Add files via upload

---
+++ camtest.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+# all_camera_idx_available = []
+
+# for camera_idx in range(10):
+#     cap = cv2.VideoCapture(camera_idx)
+#     if cap.isOpened():
+#         print(f'Camera index available: {camera_idx}')
+#         all_camera_idx_available.append(camera_idx)
+#         cap.release()
+vid = cv2.VideoCapture(0)
+
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
